adf1cbd6fb7e012ed8a4549788e64b0a8ca90f27
Francisco Soares <fmssn@cin.ufpe.br>
2026-05-11 15:28:40 -0300

    Initial Commit

---
+++ BookTrackerPOC/BookTrackerPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookTrackerPOCApp.swift
+//  BookTrackerPOC
+//
+//  Created by Francisco Miranda Soares on 11/05/26.
+//
+
+import SwiftUI
+
+@main
+struct BookTrackerPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
